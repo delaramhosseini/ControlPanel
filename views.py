513959f045cf5122b6a51a7410c93ec4797e1672
delaramhosseini <79915730+delaramhosseini@users.noreply.github.com>
2021-03-02 20:34:46 +0330

    Add files via upload

--- views.py
+++ views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse('Hello,'
+                        ' welcome to control panel,'
+                        ' this is the home page')
 # Create your views here.
