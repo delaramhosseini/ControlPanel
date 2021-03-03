4a5d5f39ed1fcb9478507e7b9970a4d6ce94b909
delaramhosseini <79915730+delaramhosseini@users.noreply.github.com>
2021-03-03 15:01:25 +0330

    Add files via upload

--- views.py
+++ views.py
@@ -1,33 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.template import loader
-from .models import Question
-from django.http import Http404
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_data')[:5]
-    template = loader.get_template('app1/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'app1/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'app1/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_data__lte=timezone.now()).order_by('-pub_data')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'app1/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_data__lte=timezone.now())
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'app1/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'app1/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('app1:results', args=(question.id,)))
