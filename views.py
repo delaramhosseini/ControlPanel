a6f8cbb62cef41035dad965dda0553f83586aef1
delaramhosseini <79915730+delaramhosseini@users.noreply.github.com>
2021-03-04 21:30:48 +0330

    Add files via upload

--- views.py
+++ views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-
+from django.contrib.auth.models import User
+from django.contrib import auth,redirects
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 class IndexView(generic.ListView):
     template_name = 'app1/index.html'
@@ -32,11 +35,33 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'app1/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+        return render(request, 'app1/detail.html', {'question': question,'error_message': "You didn't select a choice.",})
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('app1:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('app1:login'))
+
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirects('allproducts')
+        else:
+            return render(request, 'app1/login.html', {'error':'Invalid Username Or Password'})
+    else:
+        return render(request, 'app1/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirects('index')
+
+def upload(request):
+    if request.method=='post':
+        uploaded_file= request.FILES['document']
+        fs= FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+    return render(request, 'app1/upload.html')
